1cb1f03f4651cd9d7151f008b85f3f1ee61dcb8c
ErikSchuetze <erik.schuetze@hotmail.com>
2024-10-31 07:51:18 -0300

    compatibilidade

--- cancer.py
+++ cancer.py
@@ -1,18 +1,22 @@
 import os
-import webbrowser
-from wget import download
 from random import randint
 from shutil import copyfile
 
 link = 'https://thumbs.dreamstime.com/b/bandeira-de-china-com-cara-de-mao-zedong-58366447.jpg'
 arquivo= "angela.jpg"
-download(link, arquivo)
-webbrowser.open("https://youtu.be/dQw4w9WgXcQ?si=lt9mBTMUDjOxOvta")
+if os.name == "nt":
+    import webbrowser
+    from wget import download
+    download(link, arquivo)
+    webbrowser.open("https://youtu.be/uHgt8giw1LY?si=khDbHHpMKIfyGjZW")
+else:
+    os.system("firefox https://youtu.be/uHgt8giw1LY?si=khDbHHpMKIfyGjZW")
+    os.system(f"wget -O {arquivo} {link}")
 i = 0
 ii = randint(27,100)
 while True:
     copyfile("angela.jpg", os.path.join(f"angela({i}).jpg"))
     i = i+1
     if i==ii:
-        os.remove("cancer.exe")
+        #os.remove("cancer.py")
         break
